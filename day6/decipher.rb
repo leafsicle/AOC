f285e0506b24beecced98c33b2777c4fbcb74fb8
MattCooke <cookem529@gmail.com>
2022-12-06 22:22:25 -0500

    works with selection now!

--- day6/decipher.rb
+++ day6/decipher.rb
@@ -1,32 +1,67 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+require 'io/console'
+
 class Tuner
   def initialize(data)
     @data = parse(data)
   end
 
   def parse(input)
-    input.map(&:chars).flatten # may need the flatten
+    input.map(&:chars)[0] # may need the flatten
   end
 
-  def start_of_packet_detector(signals)
+  def start_of_packet_detector(signals, limit)
     viewport = []
-    signals.each_with_index do |signal, _index|
-      if viewport.size < 4
+    signals.each_with_index do |signal, index|
+      if viewport.size < limit
         viewport << signal
       else
         viewport.shift
         viewport << signal
       end
-      return _index + 1 if viewport == viewport.uniq && viewport.size == 4
+      return index + 1 if viewport == viewport.uniq && viewport.size == limit
     end
   end
 
-  def solve1
-    p start_of_packet_detector @data
+  def solve_part_1
+    p "part 1: #{start_of_packet_detector(@data, 4)}"
+  end
+
+  def solve_part_2
+    p "part 2: #{start_of_packet_detector(@data, 14)}"
   end
 end
 
-solver = Tuner.new(ARGF.readlines)
-solver.solve1
+def file_list
+  Dir.glob('*.txt')
+end
+
+def show_files
+  catcher = file_list
+  catcher.each_with_index do |entry, index|
+    puts "#{index + 1}: #{entry}"
+  end
+end
+
+def main
+  show_files
+  puts 'Against which file do you want to test your solution?'
+  file_choice = gets.chomp
+
+  if file_choice.to_i > file_list.length
+    $stdout.clear_screen
+    file_choice = nil
+    puts 'Invalid choice. Try again.'
+    exit 1
+  else
+    file_name = file_list[file_choice.to_i - 1]
+  end
+
+  file_data = File.open(file_name)
+  solver = Tuner.new(file_data)
+  solver.solve_part_1
+  solver.solve_part_2
+end
+main
