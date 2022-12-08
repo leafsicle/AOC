fdccc63058e73e567219ff4f76edb6cb4cf6d4dd
Matt C <cookem529@gmail.com>
2022-12-08 09:45:54 -0500

    only taking the sides now

--- day8/evertree.rb
+++ day8/evertree.rb
@@ -7,7 +7,7 @@ class Lookout
   def initialize(data)
     @data = data
     @forest_grid = drone_footage(@data)
-    p @forest_grid
+    count_trees @forest_grid
   end
 
   def drone_footage(input)
@@ -17,6 +17,22 @@ class Lookout
       forest_grid << playbook
     end
     forest_grid
+  end
+end
+
+def count_trees(forest)
+  # I need to write a method that accepts a 2 dimensional array and returns true if that tree is taller than 's 4 coordinates
+  # we don't need to consider the FIRST OR LAST ROWS OR COLUMNS
+  # unless index !=0 && index != forest_grid.length - 1 && line_index != 0 && line_index != line.length - 1
+  p forest
+  forest.each_with_index do |line, index|
+    next if index == 0 || index == forest.length - 1
+
+    line.each_with_index do |tree, line_index|
+      next if line_index == 0 || line_index == line.length - 1
+
+      p tree
+    end
   end
 end
 
