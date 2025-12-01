514be7d8248e4ea2efc71426543e4b107b16185c
Matt Cooke <cookem529@gmail.com>
2025-11-30 21:35:31 -0600

    part I

---
+++ 2024/day3/solution_test.rb
@@ -0,0 +1,13 @@
+require "minitest/autorun"
+require_relative "./solution"
+
+class SolutionTest < Minitest::Test
+  def test_part1_with_example
+    expected = 161
+    assert_equal expected, solve_part1("example.txt")
+  end
+
+  def test_part2_with_example
+    # assert_equal expected, solve_part2("example.txt")
+  end
+end
