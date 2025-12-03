d84884be8c78364666dd33b9071c82a0a9c48be8
Matt Cooke <cookem529@gmail.com>
2025-12-03 17:05:15 -0600

    upd day3

---
+++ 2025/day3/solution.rb
@@ -0,0 +1,12 @@
+def solve_part1(filename)
+  # TODO
+end
+
+def solve_part2(filename)
+  # TODO
+end
+
+if __FILE__ == $0
+  puts "Part 1: #{solve_part1("day3/input.txt")}"
+  puts "Part 2: #{solve_part2("day3/input.txt")}"
+end
