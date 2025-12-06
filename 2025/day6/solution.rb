6acd65e625e50a2e227785c7f23d42bb459858e9
Matt Cooke <cookem529@gmail.com>
2025-12-06 02:18:18 -0600

    pause

--- 2025/day6/solution.rb
+++ 2025/day6/solution.rb
@@ -1,40 +1,50 @@
 def solve_part1(filename)
-  sheet = []
-  File.readlines(filename).each do |line|
-    ranges = line.strip.split(" ")
-    sheet << ranges
-  end
-
-  # Remove empty lines
-  sheet.reject! { |row| row.empty? }
-
-  # Last row contains operators
-  operators = sheet.pop
-
+  sheet, operators = process_file(filename)
   # Convert all numbers to integers
   sheet.map! { |row| row.map(&:to_i) }
 
   # Process each column
   cumulative_result = 0
 
+  # since operators are in the last row, we can iterate through the operators count
   (0...operators.length).each do |col_idx|
     # Get all numbers in this column
     column_numbers = sheet.map { |row| row[col_idx] }
 
     # Apply the operator for this column
-    result = column_numbers[0]
-    (1...column_numbers.length).each do |i|
-      if operators[col_idx] == "*"
-        result *= column_numbers[i]
-      else
-        result += column_numbers[i]
-      end
-    end
+    result = operate(column_numbers, operators[col_idx])
 
     cumulative_result += result
   end
 
   cumulative_result
+end
+
+def process_file(filename)
+  sheet = []
+  File.readlines(filename).each do |line|
+    ranges = line.strip.split(" ")
+    sheet << ranges
+  end
+
+  sheet.reject! { |row| row.empty? }
+
+  # Last row contains operators
+  operators = sheet.pop
+
+  [sheet, operators]
+end
+
+def operate(numbers, operator)
+  result = numbers[0]
+  (1...numbers.length).each do |i|
+    if operator == "*"
+      result *= numbers[i]
+    else
+      result += numbers[i]
+    end
+  end
+  result
 end
 
 def solve_part2(filename)
